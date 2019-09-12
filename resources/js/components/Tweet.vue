0710b31d7b209ee678dff1cfd3f98c547ebf4c1f
pmo23 <peaceminusone23@yahoo.co.jp>
2019-09-12 15:12:03 +0900

    ツイート一覧

---
+++ resources/js/components/Tweet.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="col">
+    <nav class="panel panel-default">
+      <div class="list-group">
+        <img src="" alt="icon" class="img-circle">
+        <RouterLink
+          class="tweet__overlay"
+          :to="`/tweets/${item.id}`"
+          :title="`View the tweet by ${item.owner.name}`"
+        >
+          <div class="list-group-item">
+            <span>
+              {{ item.owner.name }}さん
+            </span>
+            <span>
+              {{ item.text }}
+            </span>
+          </div>
+        </RouterLink>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .rounded-circle {
+    display: inline;
+  }
+  .tweet__overlay {
+    display: inline;
+  }
+</style>
