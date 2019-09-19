9dd9ed528e8418040a34861bb588249711f65de3
pmo23 <peaceminusone23@yahoo.co.jp>
2019-09-19 14:47:37 +0900

    edit,update追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,11 +24,33 @@ class UserController extends Controller
     public function show(string $id)
     {
         $user = User::where('id', $id)->with(['tweets'])->first();
-        // $user = User::where('id', $id)->first();
-        // $tweets = $user->tweets()->get();
 
         return $user ?? abort(404);
-        // return $tweets ?? abort(404);
-        // return [$user, $tweets];
+    }
+
+    /**
+     * user編集
+     * @param string $id
+     * @return User
+     */
+    public function edit(string $id)
+    {
+        $user = User::where('id', $id)->first();
+
+        return $user ?? abort(404);
+    }
+
+    /**
+     * user保存
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->save();
+        return response($user, 201);
     }
 }
